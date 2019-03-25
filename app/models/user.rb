02c98dd95be6b3e9d0fba7e0e6f1b9e9f441751e
quangphu <nguyen.quang.phu@sun-asterisk.com>
2019-03-25 21:28:09 +0700

    sua conflict updating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   before_save {email.downcase!}
   validates :name, presence: true, length:{maximum: Settings.fifty}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -8,6 +10,7 @@ class User < ApplicationRecord
     uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.six}
+  validates :password, presence: true, length: {minimum: Settings.six}, allow_nil: true
 
   class << self
     def digest(string)
@@ -23,15 +26,27 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update(:id, remember_digest: User.digest(remember_token))
+    update remember_digest: User.digest(remember_token)
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
